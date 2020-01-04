e0d7ac7d8fc27f5a12937dd0b0a0d6cdb3b8eb09
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 11:25:56 +0800

    取得位置

--- myMapApp/ViewController.swift
+++ myMapApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mymap: MKMapView!
     var locationMgr:CLLocationManager! = nil
@@ -17,33 +17,59 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //取得位置
+        //取得位置授權
         locationMgr = CLLocationManager()
-        locationMgr.requestWhenInUseAuthorization()
+        locationMgr?.requestWhenInUseAuthorization()
+
+        locationMgr?.delegate = self
+        
+        locationMgr?.desiredAccuracy = kCLLocationAccuracyBest
+        locationMgr?.activityType = .automotiveNavigation
+        locationMgr?.startUpdatingLocation()
+
+        mymap.userTrackingMode = .followWithHeading
+        
         
         mymap.isScrollEnabled = true
         mymap.isZoomEnabled = true
         mymap.isRotateEnabled = false
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            let latitude:CLLocationDegrees = 25.0444032
-            let longitude:CLLocationDegrees = 121.5141468
-            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-            let xScale:CLLocationDegrees = 0.01
-            let yScale:CLLocationDegrees = 0.01
-            let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
-            let region:MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: span)
-            self.mymap.setRegion(region, animated: true)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = location
-                annotation.title = "譯智"
-                annotation.subtitle = "教育訓練中心"
-                self.mymap.addAnnotation(annotation)
-            }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            let latitude:CLLocationDegrees = 25.0444032
+//            let longitude:CLLocationDegrees = 121.5141468
+//            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+//            let xScale:CLLocationDegrees = 0.01
+//            let yScale:CLLocationDegrees = 0.01
+//            let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+//            let region:MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: span)
+//            self.mymap.setRegion(region, animated: true)
+//
+//            //取得位置
+//            if let location = self.locationMgr?.location?.coordinate {
+////                let xScale:CLLocationDegrees = 0.01
+////                let yScale:CLLocationDegrees = 0.01
+////                let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+////                print("location: \(location)")
+////                let region:MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: span)
+////                self.mymap.setRegion(region, animated: true)
+//
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                    let annotation = MKPointAnnotation()
+//                    annotation.coordinate = location
+//                    annotation.title = "譯智"
+//                    annotation.subtitle = "教育訓練中心"
+//                    self.mymap.addAnnotation(annotation)
+//                }
+//            }
             
             
-        }
+//        }
         
+    }
+    
+    //MARK: Location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let coordinate = locations[0].coordinate
+        print("coordinate: \(coordinate)")
     }
 
     @IBAction func typeChange(_ sender: UISegmentedControl) {
